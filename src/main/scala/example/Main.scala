34ecdd4ef37784191ae472c7ef1f7b9e0631233a
Duc Duong <minhduc16032004@gmail.com>
2024-05-04 00:03:11 +0300

    api-setup

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,7 +1,20 @@
 package example
+import java.net.{HttpURLConnection, URL}
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val API_KEY = "yourapikey"
+    val api_URL = "https://data.fingrid.fi/api/notifications/active"
+    val url = new URL(api_URL)
+    val conn = url.openConnection().asInstanceOf[HttpURLConnection]
+
+    conn.setRequestMethod("GET")
+    conn.setRequestProperty("Cache-Control", "no-cache")
+    conn.setRequestProperty("x-api-key",API_KEY)
+    val response = conn.getResponseCode
+    println(response)
   }
+
 }
---
+++ src/main/scala/example/RetrieveData.scala
@@ -0,0 +1,7 @@
+package example
+import java.net.{HttpURLConnection, URL}
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+object RetrieveData {
+}
