1950f198cadc35a567314195d363d234554aae71
Mattias <106891644+Neniuk@users.noreply.github.com>
2024-05-04 17:47:14 +0300

    .

--- src/main/scala/reps/datacollection/EnergyGenerationDataCollection.scala
+++ src/main/scala/reps/datacollection/EnergyGenerationDataCollection.scala
@@ -3,33 +3,70 @@ package reps.datacollection
 import java.net.{HttpURLConnection, URL}
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.io.FileWriter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import org.json4s._
+import org.json4s.native.JsonMethods._
+
+import scala.annotation.unused
 
 // Duc Duong
 // Mattias Slotte
 // Mengshi Qi
 
 object EnergyGenerationDataCollection {
-  def getKey: String = {
+  implicit val formats: DefaultFormats.type = DefaultFormats
+
+  private def getKey: String = {
     val source = scala.io.Source.fromFile("src/main/scala/reps/.env")
     val key = try source.mkString finally source.close()
     key
   }
 
-  def main(args: Array[String]): Unit = {
+  def fetchEnergyData: Unit = {
     val ApiKey = getKey
     println(ApiKey)
 
+    val date = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE)
+
+    val apiTestUrl = "https://data.fingrid.fi/api/datasets/75/data"
     val apiHydroUrl = "https://data.fingrid.fi/api/datasets/191/data"
     val apiSolarUrl = "https://data.fingrid.fi/api/datasets/248/data"
     val apiWindUrl = "https://data.fingrid.fi/api/datasets/181/data"
-    
-    val url = new URL(apiHydroUrl)
+
+    val url = new URL(apiTestUrl)
     val conn = url.openConnection().asInstanceOf[HttpURLConnection]
 
     conn.setRequestMethod("GET")
     conn.setRequestProperty("Cache-Control", "no-cache")
     conn.setRequestProperty("x-api-key",ApiKey)
-    val response = conn.getResponseCode
-    println(response)
+
+    val responseCode = conn.getResponseCode
+    if (responseCode == HttpURLConnection.HTTP_OK) {
+      val in = new BufferedReader(new InputStreamReader(conn.getInputStream))
+      val response = new StringBuilder
+      var inputLine = in.readLine()
+      while (inputLine != null) {
+        response.append(inputLine)
+        inputLine = in.readLine()
+      }
+      in.close()
+
+      val json = parse(response.toString)
+      val data = (json \ "data").extract[List[JValue]]
+      val csvFile = new FileWriter("data.csv")
+      csvFile.write("startTime,endTime,value\n")
+      data.foreach { item =>
+        val startTime = (item \ "startTime").extract[String]
+        val endTime = (item \ "endTime").extract[String]
+        val value = (item \ "value").extract[Double]
+        csvFile.write(s"$startTime,$endTime,$value\n")
+      }
+      csvFile.close()
+      println("CSV file created successfully.")
+    } else {
+      println(s"Failed to retrieve data. Response code: $responseCode")
+    }
   }
 }
