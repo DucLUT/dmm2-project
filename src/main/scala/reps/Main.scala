0b3d79fe9ed931190a0811238a0ff9de1aa8a4dc
Duc Duong <minhduc16032004@gmail.com>
2024-05-04 19:12:57 +0300

    add menu options

--- src/main/scala/reps/Main.scala
+++ src/main/scala/reps/Main.scala
@@ -7,12 +7,37 @@ import reps.views.PowerPlantView.choice
 // Mengshi Qi
 
 object Main {
+  def menu(): Unit = {
+    println("1. View Power Plant Data")
+    println("2. Analyze Energy Generation Data")
+    println("3. Generate Alerts")
+    println("4. Exit")
+    print("Enter your choice: ")
+  }
+
+  def executeOption(option: Int): Unit = {
+    option match {
+      case 1 => choice()
+      case _ => println("Invalid choice")
+    }
+  }
   def main(args: Array[String]): Unit = {
 
     fetchEnergyData("https://data.fingrid.fi/api/datasets/191/data", "hydro.csv")
     fetchEnergyData("https://data.fingrid.fi/api/datasets/248/data", "solar.csv")
     fetchEnergyData("https://data.fingrid.fi/api/datasets/75/data", "wind.csv")
 
-    choice()
+    var continue = true
+    while (continue) {
+      menu()
+      val option = scala.io.StdIn.readInt()
+      if (option == 4) {
+        continue = false
+      } else {
+        executeOption(option)
+      }
+    }
+
+
   }
 }
