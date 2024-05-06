147b5aa279c951b908d78dfe2e5082164cbd8da1
QIMENGSHI <qms030313@outlook.com>
2024-05-06 16:25:49 +0300

    PowerPlantView update

--- src/main/scala/reps/Main.scala
+++ src/main/scala/reps/Main.scala
@@ -9,6 +9,7 @@ import scala.language.postfixOps
 import java.text.{DecimalFormat, DecimalFormatSymbols}
 import java.util.Locale
 
+
 import reps.datacollection.EnergyGenerationDataCollection.fetchEnergyData
 import reps.views.PowerPlantView.choice
 import reps.dataanalysis.EnergyGenerationDataAnalysis.analyzeData
