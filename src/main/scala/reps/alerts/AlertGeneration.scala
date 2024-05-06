797509b860c4b5690680c287560b452516a58ef4
Mattias <106891644+Neniuk@users.noreply.github.com>
2024-05-06 12:50:42 +0300

    Update AlertGeneration.scala

--- src/main/scala/reps/alerts/AlertGeneration.scala
+++ src/main/scala/reps/alerts/AlertGeneration.scala
@@ -1,9 +1,13 @@
 package reps.alerts
 
+import reps.dataanalysis.EnergyGenerationDataAnalysis.analyzeData
+
 // Duc Duong
 // Mattias Slotte
 // Mengshi Qi
 
 object AlertGeneration {
-
+  def generateAlerts(): Unit = {
+    val analyzedData: List[Array[Double]] = analyzeData("data/solar.csv", "data/wind.csv", "data/hydro.csv")
+  }
 }
